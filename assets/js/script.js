0484c8a638fb54e3b38868a3ed516b1ce3b871b9
carlalennon <99259113+carlalennon@users.noreply.github.com>
2023-08-17 17:54:37 +0100

    Draw game area on screen

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,9 +8,14 @@ var score;
 // Draw the game on screen 
 
 var playArea = {
-    canvas : document.createElement("canvas");
+    canvas : document.createElement("canvas"),
 
     start : function() {
         this.canvas.width = window.innerWidth;
+        this.canvas.width = window.innerHeight;
+        this.canvas.getContext("2d");
+
+        this.frameNo = 0;
+        this.interval = setInterval(updateGameArea, 20);
     }
 }
