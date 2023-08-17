82a82a22f2946cd07481f8ecf723c5fee1fdd397
carlalennon <99259113+carlalennon@users.noreply.github.com>
2023-08-17 17:05:06 +0100

    Place html elements into variables

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+// Event listeners
+
+let character = document.getElementById("character");
+let gameWindow = document.getElementById("game-window");
+let obstacle = document.getElementById("obstacle");
